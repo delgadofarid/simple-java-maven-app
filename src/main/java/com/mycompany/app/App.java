1f4fa875e34dca742cd8de643919a7ec3143324b
Farid Delgado Molina <fdelgado@Farids-MacBook-Pro.local>
2019-04-15 23:51:40 -0400

    Solved problems

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,9 +11,6 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        int one = 1;
-        int zero = 0;
-        int result = one / 0;
         System.out.println(new App().getMessage());
     }
 
