2566628b604b4e8f0a47dbb4df81cd484c6a1efb
Farid Delgado Molina <fdelgado@Farids-MacBook-Pro.local>
2019-04-15 23:21:30 -0400

    Force failure

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,9 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
+        int one = 1;
+        int zero = 0;
+        int result = one / 0;
         System.out.println(new App().getMessage());
     }
 
